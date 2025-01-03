7f58dffd19ac73a8b69ce9ece459a0b50b189e21
nishanth1104 <nishanthayyala@gmail.com>
2025-01-03 12:06:40 -0500

    removed awards

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import Education from "./components/Education";
-import CertificatesAndAwards from "./components";
 const App = () => {
   return (
     <BrowserRouter>
@@ -16,7 +15,7 @@ const App = () => {
         <Tech />
         <Education />
         <Works />
-        <CertificatesAndAwards />
+        
 
         <div className='relative z-0'>
           <Contact />
