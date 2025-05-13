9b253de87f9ea28b6402d93bd9e87aa343e988e2
nishanth1104 <nishanthayyala@gmail.com>
2025-05-12 23:15:20 -0400

    Role Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Nishanth Ayyalasomayajula &nbsp;
-            <span className='sm:block hidden'> | Graduate Student</span>
+            <span className='sm:block hidden'> | LLM Engineer</span>
           </p>
         </Link>
 
